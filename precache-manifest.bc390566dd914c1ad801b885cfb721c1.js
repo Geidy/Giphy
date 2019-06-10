a36c300b21fb9fb75c7f028ce26ee5de5a50a015
Geidy <geidydm@gmail.com>
2019-06-10 05:43:42 -0400

    Updates

---
+++ precache-manifest.bc390566dd914c1ad801b885cfb721c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba993cf95b85b8e32af8511cb7fc1d99",
+    "url": "/Giphy/index.html"
+  },
+  {
+    "revision": "f92b945a8b658632632f",
+    "url": "/Giphy/static/css/main.f6d93967.chunk.css"
+  },
+  {
+    "revision": "919a32d3dd1fcc926618",
+    "url": "/Giphy/static/js/2.7f82bd93.chunk.js"
+  },
+  {
+    "revision": "f92b945a8b658632632f",
+    "url": "/Giphy/static/js/main.9272e42a.chunk.js"
+  },
+  {
+    "revision": "2a62d14ba24ea4bac3c6",
+    "url": "/Giphy/static/js/runtime~main.1a4a087b.js"
+  }
+]);
